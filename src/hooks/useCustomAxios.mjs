c2d88f8734b05f7d7f333c8234d3dd224e8024fe
mackh <kopserf@gmail.com>
2024-03-28 16:12:59 +0900

    change about accessToken refresh

--- src/hooks/useCustomAxios.mjs
+++ src/hooks/useCustomAxios.mjs
@@ -1,15 +1,15 @@
 import { memberState } from "@recoil/user/atoms.mjs";
 import axios from "axios";
-import { useRecoilValue } from "recoil";
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useRecoilState, useRecoilValue } from "recoil";
+import { useLocation, useNavigate } from "react-router-dom";
 
 const API_SERVER = import.meta.env.VITE_API_SERVER;
-
+const REFRESH_URL = "/auth/refresh";
 function useCustomAxios() {
 	// 로그인 된 사용자 정보
-	const user = useRecoilValue(memberState);
+	const [user, setUser] = useRecoilState(memberState);
 	const navigate = useNavigate();
-  const location = useLocation();
+	const location = useLocation();
 	// ajax 통신에 사용할 공통 설정 지정
 	const instance = axios.create({
 		baseURL: API_SERVER,
@@ -23,22 +23,49 @@ function useCustomAxios() {
 	// 요청 인터셉터
 	instance.interceptors.request.use(config => {
 		if (user) {
-			const accessToken = user.token.accessToken;
-			config.headers.Authorization = `Bearer ${accessToken}`;
+			let token = user.token.accessToken;
+			if (config.url == REFRESH_URL) {
+				token = user.token.refreshToken;
+			}
+			config.headers.Authorization = `Bearer ${token}`;
 		}
 		return config;
 	});
+	// 응답 인터셉터
 	instance.interceptors.response.use(
 		res => res,
-		err => {
-      if (err.response?.status == '401') {
-          const gotoLogin = confirm("go to login?");
-          gotoLogin&&navigate('/users/login',{state:{from:location.pathname}});
-      }else{
-        return Promise.reject(err);
-      }
-    }
+		async err => {
+			const { config, response } = err;
+			if (err.response?.status === 401) {
+				// 인증 되지 않음
+				if (config.url === REFRESH_URL) {
+					//refresh 토큰 실패
+					const gotoLogin = confirm("로그인 후 이용 가능합니다.\n로그인 페이지로 이동하시겠습니까?");
+					gotoLogin && navigate("/users/login", { state: { from: location.pathname } });
+				} else {
+					//refresh토큰으로 access토큰 재발급 요청
+					const accessToken = await getAccessToken(instance);
+					if (accessToken) {
+						config.headers.Authorization = `Bearer ${accessToken}`;
+						return axios(config);
+					}
+				}
+			} else {
+				return Promise.reject(err);
+			}
+		}
 	);
+
+	async function getAccessToken(instance) {
+		try {
+			const {
+				data: { accessToken },
+			} = await instance.get(REFRESH_URL);
+			return accessToken;
+		} catch (error) {
+			console.log(error);
+		}
+	}
 	return instance;
 }
 
